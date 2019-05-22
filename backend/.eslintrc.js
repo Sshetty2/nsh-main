d8e3aa1dc9e56e4f1224a0a2b8e95305f11c91d9
Sachit Shetty @_@ Sonny <sachit.shetty@gmail.com>
2019-05-21 22:54:12 -0400

    reformat and rename

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -6,21 +6,16 @@ module.exports = {
   env: {
     node: true
   },
-  extends: [
-    'standard',
-    'prettier',
-    'prettier/standard',
-    'plugin:jest/recommended'
-  ],
+  extends: ['standard', 'prettier', 'prettier/standard', 'plugin:jest/recommended'],
   plugins: ['prettier', 'jest'],
   rules: {
     'promise/catch-or-return': 'error',
     'prettier/prettier': [
       'error',
       {
-        'singleQuote': true,
-        'semi': false
+        singleQuote: true,
+        semi: false
       }
     ]
   }
-}
+};
