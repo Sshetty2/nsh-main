832f2deecfcfdc22c24d94f6b7ae9efa15d749c2
Sachit Shetty @_@ Sonny <sachit.shetty@gmail.com>
2019-05-23 23:43:48 -0400

    add layout components to main app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,27 @@
 import React from 'react';
 
 import AppBar from './components/AppBar';
+import Grid from '@material-ui/core/Grid';
+import Footer from './components/Footer';
 
 const App: React.FC = ({ classes }: any) => {
-  return <AppBar />;
+  return (
+    <>
+      <AppBar />
+      <Grid container spacing={8}>
+        <Grid item sm={6} xl={3} xs={12}>
+          <div>Sample Text</div>
+        </Grid>
+        <Grid item sm={6} xl={3} xs={12}>
+          <div>Sample Text</div>
+        </Grid>
+        <Grid item lg={8} md={12} xl={9} xs={12}>
+          <div>Sample Text</div>
+        </Grid>
+      </Grid>
+      <Footer />
+    </>
+  );
 };
 
 export default App;
