482d72fac4e5162396e9e0697f3b2c3f54e81aef
Sachit Shetty @_@ Sonny <sachit.shetty@gmail.com>
2019-05-23 21:07:28 -0400

    include theme provider and wrap app

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,7 +4,28 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { MuiThemeProvider } from '@material-ui/core/styles';
+import theme from './theme';
+
+// import { composeWithDevTools } from 'redux-devtools-extension';
+// import { createStore, applyMiddleware } from 'redux';
+// import { Provider } from 'react-redux';
+// import thunk from 'redux-thunk';
+
+// import { BrowserRouter } from 'react-router-dom';
+// import { rootReducer } from './reducers';
+
+const mainApp = (
+  <MuiThemeProvider theme={theme}>
+    {/* <Provider store={store}>
+      <BrowserRouter> */}
+    <App />
+    {/* </BrowserRouter>
+    </Provider> */}
+  </MuiThemeProvider>
+);
+
+ReactDOM.render(mainApp, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
