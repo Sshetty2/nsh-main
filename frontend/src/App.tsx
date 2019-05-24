2887e1931a7c286f918a5e06d6eb252bec3135cd
Sachit Shetty @_@ Sonny <sachit.shetty@gmail.com>
2019-05-23 21:08:18 -0400

    include app bar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,57 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-const App: React.FC = () => {
+import { withStyles } from '@material-ui/core/styles';
+
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+const styles = (theme: any) => ({
+  root: {
+    backgroundColor: theme.palette.common.white,
+
+    alignItems: 'center',
+    height: '64px',
+    zIndex: theme.zIndex.appBar,
+    flexGrow: 1
+  },
+  toolbar: {
+    minHeight: 'auto',
+    width: '100%'
+  },
+  title: {
+    marginLeft: theme.spacing.unit,
+    flexGrow: 1
+  },
+  menuButton: {
+    marginLeft: '-4px',
+    marginRight: theme.spacing.unit * 2
+  },
+  notificationsButton: {
+    marginLeft: 'auto'
+  },
+  signOutButton: {
+    marginLeft: theme.spacing.unit
+  }
+});
+
+const App: React.FC = ({ classes }: any) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppBar className={classes.root} position="static">
+      <Toolbar>
+        <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6" className={classes.title}>
+          News
+        </Typography>
+        <Button color="inherit">Login</Button>
+      </Toolbar>
+    </AppBar>
   );
-}
+};
 
-export default App;
+export default withStyles(styles)(App);
