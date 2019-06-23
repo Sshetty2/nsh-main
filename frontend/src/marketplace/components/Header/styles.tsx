06f54eb812b0ea11a92af1f8f5acc9cd11e8c52b
Sachit Shetty @_@ Sonny <sachit.shetty@gmail.com>
2019-06-23 19:03:35 -0400

    update header styling

--- frontend/src/marketplace/components/Header/styles.tsx
+++ frontend/src/marketplace/components/Header/styles.tsx
@@ -1,30 +1,31 @@
 export const styles = (theme: any) => ({
-  root: {
-    backgroundColor: theme.palette.common.white,
-    alignItems: 'center',
-    height: '7.5vh',
-    zIndex: theme.zIndex.appBar,
-    display: 'flex'
-  },
-  toolbar: {
-    height: '100%',
-    minHeight: 'auto',
-    width: '100%'
-  },
-  title: {
-    marginLeft: theme.spacing.unit,
-    marginRight: theme.spacing.unit,
-    minWidth: '175px'
-  },
-  clientsButton: {
-    marginLeft: theme.spacing.unit * 2
-  },
-  candidatesButton: {
-    marginLeft: 'auto'
-  },
-  signOutButton: {
-    marginLeft: theme.spacing.unit * 2
-  }
-});
+         root: {
+           backgroundColor: theme.palette.common.white,
+           alignItems: 'center',
+           height: '6vh',
+           zIndex: theme.zIndex.appBar,
+           display: 'flex',
+           backgroundImage: 'radial-gradient(circle farthest-corner at 50% 50%, #fff 5%, #eef8ff)'
+         },
+         toolbar: {
+           height: '100%',
+           minHeight: 'auto',
+           width: '100%'
+         },
+         title: {
+           marginLeft: theme.spacing.unit,
+           marginRight: theme.spacing.unit,
+           minWidth: '175px'
+         },
+         clientsButton: {
+           marginLeft: theme.spacing.unit * 2
+         },
+         candidatesButton: {
+           marginLeft: 'auto'
+         },
+         signOutButton: {
+           marginLeft: theme.spacing.unit * 2
+         }
+       });
 
 export default styles;
